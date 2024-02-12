2d2460d7b2f11fbf8f8672e8bde69ee2dcff883c
ydiv6013 <yogesh.dhameliya@outlook.com>
2024-02-12 15:27:33 +0000

    updating get files code

--- Convolutional_nueral_net/Pizza,samosa classificaion/get_files.py
+++ Convolutional_nueral_net/Pizza,samosa classificaion/get_files.py
@@ -1,5 +1,20 @@
 import os
+import cv2 as cv
 
+path = "/Users/yogesh/pythoncode/datasets/pizza-samosa/pizza/"
+image = "2965.jpg"
+url = str(path) + str(image)
+
+print(url)
+
+img = cv.imread(url)
+"""
+if img is None :
+    print("Opps something went wrong")
+else :"""
+cv.imshow("orignal image",image)
+cv.waitKey()
+cv.destroyAllWindows()
 
 class GetFiles :
     def __init__(self) -> None:
@@ -9,6 +24,7 @@ class GetFiles :
 
         filepath = filepath
         dirpath,dirname,filename = os.walk(filepath) 
+        print("name of the directory is : \n",dirname)
        
 
         dir1 = str(dirpath) + "/" + "samosa"
@@ -16,6 +32,15 @@ class GetFiles :
 
         print(dir1)
         print(os.path)
+        
+        for files in filename :
+            print(files)
+            urls1 = str(dir1) + str(files)
+        
+        print("\n urls are : \n" ,urls1)
+        print("\n Files are :\n" ,files)
+        
+
 
 
 
@@ -25,4 +50,4 @@ if __name__ == "__main__" :
     image_datasets_path  = "/Users/yogesh/pythoncode/datasets/pizza-samosa/"
 
     gf = GetFiles()
-    gf.get_files(filepath=image_datasets_path)
+    #gf.get_files(filepath=image_datasets_path)
